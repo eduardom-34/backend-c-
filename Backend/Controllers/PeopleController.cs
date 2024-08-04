2316577a42801a860dfc78e88365d7968a4754e7
eduardo 34 - pc Efrain <eduardomelchor34@gmail.com>
2024-08-04 13:48:35 -0600

    Creacion de capa de servicio

--- Backend/Controllers/PeopleController.cs
+++ Backend/Controllers/PeopleController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Backend.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Backend.Controllers
@@ -7,6 +8,14 @@ namespace Backend.Controllers
     [ApiController]
     public class PeopleController : ControllerBase
     {
+
+        private IPeopleService _peopleService;
+
+        public PeopleController()
+        {
+            _peopleService = new PeopleService();
+        }
+
         [HttpGet("all")]
         public List<People> GetPeople() => Repository.People;
 
@@ -31,7 +40,7 @@ namespace Backend.Controllers
         [HttpPost]
         public IActionResult Add(People people)
         {
-            if( string.IsNullOrEmpty(people.Name))
+            if( !_peopleService.Validate(people))
             {
                 return BadRequest();
             }
