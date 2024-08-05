652899ecef7a0fa14eabb25bbddb7ab02b18e550
eduardo 34 - pc Efrain <eduardomelchor34@gmail.com>
2024-08-04 18:00:46 -0600

    Creacion de metodo sincrono

---
+++ Backend/Controllers/SomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SomeController : ControllerBase
+    {
+        [HttpGet("sync")]
+        public IActionResult GetSync()
+        {
+
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            stopwatch.Start();
+
+            Thread.Sleep(1000);
+            Console.WriteLine("Conexion a base de datos terminada");
+            
+            Thread.Sleep(1000);
+            Console.WriteLine("Envio de email terminado");
+
+            Console.WriteLine("Todo ha terminado");
+            stopwatch.Stop();
+            return Ok(stopwatch.Elapsed);
+
+        }
+    }
+}
